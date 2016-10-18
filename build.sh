294ca3b70e46fa75b21fa41c537d571591db5d3d
Dustin Moris Gorski <dustinmoris@gmail.com>
2016-10-18 19:53:05 +0100

    Split the build and deployent into two scripts

--- build.sh
+++ build.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-
-# Stop the script if a command returns a non 0 exit code
 set -ev
-
-# Check if the variables exist otherwise read from shell arguments
-TRAVIS_TAG=${TRAVIS_TAG:-$1}
-DOCKER_USERNAME=${DOCKER_USERNAME:-$2}
-DOCKER_PASSWORD=${DOCKER_PASSWORD:-$3}
 
 # ---------------------------------
 # BUILD
@@ -14,37 +7,3 @@ DOCKER_PASSWORD=${DOCKER_PASSWORD:-$3}
 
 dotnet restore
 dotnet build -c Release src
-dotnet publish -c Release src
-
-# ---------------------------------
-# DEPLOY
-# ---------------------------------
-
-# Only publish to Docker Hub on tags
-if [ -n "$TRAVIS_TAG" ]; then
-
-    # Read the tag name into an array split by the dot (.)
-    IFS='.' read -r -a tag <<< "$TRAVIS_TAG"
-
-    # Remove a leading v from the major version number (e.g. if the tag was v1.0.0)
-    MAJOR="${tag[0]//v}"
-
-    # Set the other parts of the sem ver to respective variables as well
-    MINOR=${tag[1]}
-    BUILD=${tag[2]}
-
-    # Create a clean sem ver variable
-    SEMVER="$MAJOR.$MINOR.$BUILD"
-
-    # Build the Docker image
-    docker build -t dustinmoris/newshacker:$SEMVER src/bin/Release/netcoreapp1.0/publish/.
-
-    # Tag the same image with :latest as well
-    docker tag dustinmoris/newshacker:$SEMVER dustinmoris/newshacker:latest
-
-    # Login to Docker Hub and upload images
-    docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-    docker push dustinmoris/newshacker:$SEMVER
-    docker push dustinmoris/newshacker:latest
-    
-fi
---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -ev
+
+# ---------------------------------
+# DEPLOY
+# ---------------------------------
+
+TAG=$1
+DOCKER_USERNAME=$2
+DOCKER_PASSWORD=$3
+
+# Create publish artifact
+dotnet publish -c Release src
+
+# Remove a leading v from the major version number (e.g. if the tag was v1.0.0)
+IFS='.' read -r -a tag_array <<< "$TAG"
+MAJOR="${tag_array[0]//v}"
+MINOR=${tag_array[1]}
+BUILD=${tag_array[2]}
+SEMVER="$MAJOR.$MINOR.$BUILD"
+
+# Build the Docker images
+docker build -t dustinmoris/newshacker:$SEMVER src/bin/Release/netcoreapp1.0/publish/.
+docker tag dustinmoris/newshacker:$SEMVER dustinmoris/newshacker:latest
+
+# Login to Docker Hub and upload images
+docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+docker push dustinmoris/newshacker:$SEMVER
+docker push dustinmoris/newshacker:latest
